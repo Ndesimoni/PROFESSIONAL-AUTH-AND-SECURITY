a054dfed505b7e1800e17ff2beda7ae6d0cdc4d4
nde booy <ndesimoniche@gmail.com>
2023-12-30 17:47:53 +0400

    creat data base with mongo db and connect our project to it

--- APIs/index.js
+++ APIs/index.js
@@ -1,4 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const Dotenv = require("dotenv");
+
+Dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("data base connected");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 const app = express();
 
