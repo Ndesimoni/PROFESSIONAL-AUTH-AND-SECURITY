ab4a2f2d5f88d6a29631c84ce5f4a9ecff24384b
nde booy <ndesimoniche@gmail.com>
2024-01-01 20:24:10 +0400

    password hashed problem solved

--- APIs/controlers/Auth.controler.js
+++ APIs/controlers/Auth.controler.js
@@ -1,13 +1,13 @@
 const User = require("../model/user_model.js");
-// const bcryptjs = require("bcryptjs");
+const bcryptjs = require("bcryptjs");
 
 const signup = async (req, res) => {
   const { username, email, password } = req.body;
 
-  //password hashing with bcrypt not implimented so, we would look at the code again
-  // const hashPassword = bcrypt.hashSync(password, 10);
+  // password hashing with bcrypt not implimented so, we would look at the code again
+  const hashPassword = bcryptjs.hashSync(password, 10);
 
-  const newUser = new User({ username, email, password });
+  const newUser = new User({ username, email, password: hashPassword });
   console.log(newUser);
   try {
     await newUser.save();
